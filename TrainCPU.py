25dcce9ab1734046c29a0b08b78ce8a8b61395cb
Amir Gohari <140350718+AmirMG47@users.noreply.github.com>
2025-12-16 02:46:11 +0330

    TrainCPU.py

--- TrainCPU.py
+++ TrainCPU.py
@@ -18,13 +18,3 @@ print(' Training complete')
 
 
 
-#model.train(data="Guitar_v8.yaml",epoch = 5, imgsz=640,dryrun=True)
-
-
-
-#Amuzeshe model
-#yolo task=detect mode=train model=yolov8s.pt data=C:/Users/Amir/Desktop/Yolo8Guitar/Guitar_dataset/Guitar_v8.yaml epochs=5 imgsz=640 batch=16
-
-
-#arzyabi model
-#yolo task=detect mode=val model=runs/detect/train/weights/best.pt data=C:/Users/Amir/Desktop/Yolo8Guitar/Guitar_dataset/Guitar_v8.yaml
