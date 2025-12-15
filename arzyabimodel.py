92c118f7216f26a76ff105b9a81c154a4f6fb349
Amir Gohari <140350718+AmirMG47@users.noreply.github.com>
2025-12-16 02:48:13 +0330

    arzyabimodel.py

--- arzyabimodel.py
+++ arzyabimodel.py
@@ -1,8 +1,8 @@
 # تصویر تستی
 from ultralytics import YOLO
 
-model = YOLO('C:/Users/Amir/Desktop/Yolo8 Guitar/guitar_detector/weights/best.pt')
-test_image = r"C:/Users/Amir/Desktop/Yolo8 Guitar/1.jpg"
+model = YOLO('C:/Users/---your path---/best.pt')
+test_image = r"C:/Users/---image path---/1.jpg"
 predict_results = model.predict(
     source=test_image,
     save=True,          # خروجی را در runs ذخیره می‌کند
